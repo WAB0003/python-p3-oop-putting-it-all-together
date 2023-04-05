8880746587dcb3525c0420511fe085be3693873c
Bill Brown <bill.a.brown90@gmail.com>
2023-04-05 16:57:13 -0700

    attributes and properties

--- lib/book.py
+++ lib/book.py
@@ -1,4 +1,27 @@
 #!/usr/bin/env python3
+#pytest lib/testing/book_test.py -x
+import ipdb
 
 class Book:
-    pass
+    def __init__(self, title="title", author = "author", page_count = 0, genre = "genre"):
+        self.title = title
+        self.author = author
+        self.page_count = page_count
+    
+    #! Create a page count property
+    def get_page_count(self):
+        return self._page_count
+    
+    def set_page_count (self, page_count):
+        if type(page_count)==int:
+            self._page_count = page_count
+        else:
+            print("page_count must be an integer")
+    
+    page_count = property(get_page_count,set_page_count)
+    
+    #! Create a turnpage def
+    def turn_page(self):
+        print("Flipping the page...wow, you read fast!")
+    
+# ipdb.set_trace()
--- lib/shoe.py
+++ lib/shoe.py
@@ -1,4 +1,30 @@
 #!/usr/bin/env python3
 
+#pytest lib/testing/shoe_test.py -x
+
+import ipdb
+
 class Shoe:
-    pass
+    def __init__(self, brand="assign a brand", color="assign a color", size=0, material="assign material", condition="Used" ):
+        self.brand = brand
+        self.color = color
+        self.size = size
+        self.material = material
+        self.condition = condition  
+        
+    def get_size(self):
+        return self._size
+    
+    def set_size(self, size):
+        if (type(size)==int):
+            self._size = size
+        else:
+            "size must be an integer"
+        
+    size = property(get_size,set_size)
+    
+    def cobble(self):
+        self.condition = "New"
+        print("Your shoe is as good as new!")
+       
+# ipdb.set_trace()
